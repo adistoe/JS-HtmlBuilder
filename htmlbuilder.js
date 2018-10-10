7e58cad22f56dda351d0cc48dbc17f0c8f4b6641
adistoe <adistoe@adistoe.ch>
2018-10-10 18:41:23 +0200

    Add files
    + Add files

---
+++ htmlbuilder.js
@@ -0,0 +1,218 @@
+/**
+ * Constructor
+ *
+ * @param {object} parent - At the moment not in use (registered for later use)
+ */
+function HTMLBuilder(parent) {
+    if (parent) {
+        this.parent = parent;
+    }
+}
+
+/**
+ * Main function to create html elements
+ *
+ * @param {string} type - Element type
+ * @param {string} content - Content between the html opening and closing tag
+ * @param {string} attriubutes - Attribute array [[attribute, content], ...]
+ *
+ * @returns {object} - Created element
+ */
+HTMLBuilder.prototype.create = function(type, content, attributes) {
+    var element = document.createElement(type);
+
+    var key;
+    for (key in attributes) {
+        element.setAttribute(attributes[key][0], attributes[key][1]);
+    }
+
+    if (content) {
+        element.innerHTML = content;
+    }
+
+    return element;
+};
+
+/**
+ * Create anchor <a></a>
+ *
+ * @param {string} href - Target URL
+ * @param {string} content - Content between the html opening and closing tag
+ * @param {string} attriubutes - Attribute array [[attribute, content], ...]
+ *
+ * @returns {object} - Created element
+ */
+HTMLBuilder.prototype.createAnchor = function(href, content, attributes) {
+    if (!attributes) {
+        attributes = [];
+    }
+
+    // Add href at the beginning of the attributes array
+    attributes.unshift(['href', href]);
+
+    var element = this.create('a', content, attributes);
+
+    return element;
+};
+
+/**
+ * Create button <button></button>
+ *
+ * @param {string} text - Buttontext
+ * @param {string} attriubutes - Attribute array [[attribute, content], ...]
+ *
+ * @returns {object} - Created element
+ */
+HTMLBuilder.prototype.createButton = function(text, attributes) {
+    var element = this.create('button', text, attributes);
+
+    return element;
+};
+
+/**
+ * Create div <div></div>
+ *
+ * @param {string} content - Content between the html opening and closing tag
+ * @param {string} attriubutes - Attribute array [[attribute, content], ...]
+ *
+ * @returns {object} - Created element
+ */
+HTMLBuilder.prototype.createDiv = function(content, attributes) {
+    var element = this.create('div', content, attributes);
+
+    return element;
+};
+
+/**
+ * Create headline <h1></h1> - <h6></h6>
+ *
+ * @param {string} type - Headline type (h1 - h6)
+ * @param {string} content - Content between the html opening and closing tag
+ * @param {string} attriubutes - Attribute array [[attribute, content], ...]
+ *
+ * @returns {object} - Created element
+ */
+HTMLBuilder.prototype.createHeadline = function(type, content, attributes) {
+    var element;
+
+    switch (type) {
+        default:
+        case '1':
+        case 'h1':
+            element = this.create('h1', content, attributes);
+
+            break;
+        case '2':
+        case 'h2':
+            element = this.create('h2', content, attributes);
+
+            break;
+        case '3':
+        case 'h3':
+            element = this.create('h3', content, attributes);
+
+            break;
+        case '4':
+        case 'h4':
+            element = this.create('h4', content, attributes);
+
+            break;
+        case '5':
+        case 'h5':
+            element = this.create('h5', content, attributes);
+
+            break;
+        case '6':
+        case 'h6':
+            element = this.create('h6', content, attributes);
+
+            break;
+    }
+
+    return element;
+};
+
+/**
+ * Create image <img>
+ *
+ * @param {string} source - Image source URL
+ * @param {string} alt - Alternative text if image cannot be shown
+ * @param {string} attriubutes - Attribute array [[attribute, content], ...]
+ *
+ * @returns {object} - Created element
+ */
+HTMLBuilder.prototype.createImage = function(source, alt, attributes) {
+    if (!attributes) {
+        attributes = [];
+    }
+
+    // Add src and alt as first and second position in the attributes array
+    attributes.unshift(['alt', alt]);
+    attributes.unshift(['src', source]);
+
+    var element = this.create('img', null, attributes);
+
+    return element;
+};
+
+/**
+ * Create input <input>
+ *
+ * @param {string} type - Input type (button, checkbox, password, radio, submit, text...)
+ * @param {string} attriubutes - Attribute array [[attribute, content], ...]
+ *
+ * @returns {object} - Created element
+ */
+HTMLBuilder.prototype.createInput = function(type, attributes) {
+    // Check if a type is set (So it has not to be given in the attributes anymore)
+    if (type) {
+        if (!attributes) {
+            attributes = [];
+        }
+
+        // Add type at the beginning of the attributes array
+        attributes.unshift(['type', type]);
+    }
+
+    var element = this.create('input', null, attributes);
+
+    return element;
+};
+
+/**
+ * Create label <label></label>
+ *
+ * @param {string} content - Content between the html opening and closing tag
+ * @param {string} forAttr - "for" attribute for the label
+ * @param {string} attriubutes - Attribute array [[attribute, content], ...]
+ *
+ * @returns {object} - Created element
+ */
+HTMLBuilder.prototype.createLabel = function(content, forAttr, attributes) {
+    // Add "for" at the beginning of the attributes array if given
+    if (forAttr) {
+        if (!attributes) {
+            attributes = [];
+        }
+
+        attributes.unshift(['for', forAttr]);
+    }
+
+    var element = this.create('label', content, attributes);
+
+    return element;
+};
+
+/**
+ * Create paragraph <p></p>
+ *
+ * @param {string} content - Content between the html opening and closing tag
+ * @param {string} attriubutes - Attribute array [[attribute, content], ...]
+ *
+ * @returns {object} - Created element
+ */
+HTMLBuilder.prototype.createParagraph = function(content, attributes) {
+    var element = this.create('p', content, attributes);
+
+    return element;
+};
